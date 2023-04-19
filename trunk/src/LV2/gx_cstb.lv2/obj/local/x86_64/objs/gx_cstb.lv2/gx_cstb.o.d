3a8e785048d00134e5a20b0016dd21bfad8e5f4c
Shaji Khan <git@shaji.in>
2023-04-19 19:55:42 +0530

    gx_cstb.lv2

---
+++ trunk/src/LV2/gx_cstb.lv2/obj/local/x86_64/objs/gx_cstb.lv2/gx_cstb.o.d
@@ -0,0 +1,21 @@
+./obj/local/x86_64/objs/gx_cstb.lv2/gx_cstb.o: jni/gx_cstb.cpp \
+  jni/gx_common.h jni/gx_compiler.h jni/gx_bypass.cc jni/gx_cstb.h \
+  jni/gx_pluginlv2.h jni/cstb.cc jni/trany.h jni/KT88.cc jni/7199P.cc
+
+jni/gx_common.h:
+
+jni/gx_compiler.h:
+
+jni/gx_bypass.cc:
+
+jni/gx_cstb.h:
+
+jni/gx_pluginlv2.h:
+
+jni/cstb.cc:
+
+jni/trany.h:
+
+jni/KT88.cc:
+
+jni/7199P.cc:
