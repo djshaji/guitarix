51ac54818532e162f7f8e72b2a906c3c7c2020bd
Shaji Khan <git@shaji.in>
2023-04-19 20:13:50 +0530

    gx_delay.lv2

---
+++ trunk/src/LV2/gx_delay.lv2/obj/local/x86_64/objs/gx_delay.lv2/gx_delay.o.d
@@ -0,0 +1,15 @@
+./obj/local/x86_64/objs/gx_delay.lv2/gx_delay.o: jni/gx_delay.cpp \
+  jni/gx_common.h jni/gx_compiler.h jni/gx_bypass.cc jni/gx_delay.h \
+  jni/gx_pluginlv2.h jni/stereodelay.cc
+
+jni/gx_common.h:
+
+jni/gx_compiler.h:
+
+jni/gx_bypass.cc:
+
+jni/gx_delay.h:
+
+jni/gx_pluginlv2.h:
+
+jni/stereodelay.cc:
