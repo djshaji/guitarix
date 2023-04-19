499f1aee9d8f4f1fbd8bc103bb9fe4106ac50525
Shaji Khan <git@shaji.in>
2023-04-19 20:24:27 +0530

    gxechocat.lv2

---
+++ trunk/src/LV2/gxechocat.lv2/obj/local/armeabi-v7a/objs/gxechocat.lv2/gxechocat.o.d
@@ -0,0 +1,35 @@
+./obj/local/armeabi-v7a/objs/gxechocat.lv2/gxechocat.o: jni/gxechocat.cpp \
+  jni/gx_common.h jni/gx_compiler.h jni/gxechocat.h jni/gx_pluginlv2.h \
+  jni/gxechocat.cc jni/copicat1_neg_table.h jni/copicat1_table.h \
+  jni/copicatrecord_2_neg_table.h jni/copicatrecord_2_table.h \
+  jni/copicatreplay1_neg_table.h jni/copicatreplay1_table.h \
+  jni/copicatreplay2_neg_table.h jni/copicatreplay2_table.h \
+  jni/noiser.cc
+
+jni/gx_common.h:
+
+jni/gx_compiler.h:
+
+jni/gxechocat.h:
+
+jni/gx_pluginlv2.h:
+
+jni/gxechocat.cc:
+
+jni/copicat1_neg_table.h:
+
+jni/copicat1_table.h:
+
+jni/copicatrecord_2_neg_table.h:
+
+jni/copicatrecord_2_table.h:
+
+jni/copicatreplay1_neg_table.h:
+
+jni/copicatreplay1_table.h:
+
+jni/copicatreplay2_neg_table.h:
+
+jni/copicatreplay2_table.h:
+
+jni/noiser.cc:
